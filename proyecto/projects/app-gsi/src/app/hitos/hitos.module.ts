8f7a6923cec90b4fd9508a914bb8e8b591864779
Bryanovi <boviedo4550@uta.edu.ec>
2023-06-16 10:32:36 -0500

    Modulos

---
+++ proyecto/projects/app-gsi/src/app/hitos/hitos.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HitosRoutingModule } from './hitos-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    HitosRoutingModule
+  ]
+})
+export class HitosModule { }
